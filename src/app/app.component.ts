a977fbd04165e9e118e413b3a2cd8916096d5c95
l3miage-delagral <alex.del57640@gmail.com>
2025-01-06 10:25:55 +0100

    n°3 - Favoriser un design simple, épuré, adapté au web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
-export class AppComponent {}
+export class AppComponent {
+  @HostListener("document:mousemove", ["$event"])
+  onMouseMove(event: MouseEvent) {
+    const cursor = document.getElementById("animated-cursor");
+    if (cursor) {
+      cursor.style.left = `${event.clientX}px`;
+      cursor.style.top = `${event.clientY}px`;
+    }
+  }
+}
